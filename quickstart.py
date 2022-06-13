c0d6e17dac3e74fb65034d73bedba60635b17aa2
Shivang Vora <69647458+ShivangVora1206@users.noreply.github.com>
2022-06-13 15:52:00 +0530

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -12,7 +12,7 @@ from googleapiclient.errors import HttpError
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+SAMPLE_SPREADSHEET_ID = ''
 SAMPLE_RANGE_NAME = 'Class Data!A2:E'
 
 
